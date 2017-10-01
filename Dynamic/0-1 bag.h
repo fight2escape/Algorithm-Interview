350ae900046a262b3e4f858bc5b956520e275cb6
DESKTOP-P4K92SG\Shinelon <fight2escape@gmail.com>
2017-10-01 12:54:11 +0800

    DP: 0-1背包 优化

--- Dynamic/0-1 bag.h
+++ Dynamic/0-1 bag.h
@@ -14,8 +14,66 @@ public:
 
     /**
      * 0-1背包：迭代版
+     * 一行记录
+     * 时间复杂度：O(n*C)
+     * 空间复杂度：O(C)
      * @param w
-     * @param v 
+     * @param v
+     * @param c
+     * @return
+     */
+    int knapsack01(const vector<int> &w, const vector<int> &v, int c){
+        int n = w.size();
+        vector<int> res(c+1, -1);
+        for(int i=0; i<=c; i++){
+            res[i] = w[0]<c ? 0 : v[0];
+        }
+        for(int i=1; i<n; i++){
+            for(int j=w[i]; j<=c; j++){
+                res[j] = max(res[j], v[i] + res[ j-w[i] ]);
+            }
+        }
+        return res[c];
+    }
+
+    /**
+     * 0-1背包：迭代版
+     * 两行记录
+     * 时间复杂度：O(n*C)
+     * 空间复杂度：O(C)
+     * @param w
+     * @param v
+     * @param c
+     * @return
+     */
+    int knapsack01_3(const vector<int> &w, const vector<int> &v, int c){
+        int n = w.size();
+        vector<vector<int>> res(2, vector<int>(c+1, -1));
+        for(int i=0; i<=c; i++){
+            res[0][i] = i<w[0] ? 0 : v[0];
+        }
+
+        for(int i=1; i<n; i++){
+            int cur = i%2;
+            int pre = cur==0 ? 1 : 0;
+            for(int j=0; j<=c; j++){
+                if(j < w[i]){
+                    res[cur][j] = res[pre][j];
+                }else{
+                    res[cur][j] = max(res[pre][j], v[i]+res[pre][ j-w[i] ]);
+                }
+            }
+        }
+        return res[(n-1)%2][c];
+    }
+
+
+    /**
+     * 0-1背包：迭代版
+     * 时间复杂度：O(n*C)
+     * 空间复杂度：O(n*C)
+     * @param w
+     * @param v
      * @param c
      * @return
      */
