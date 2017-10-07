894272512a466d36e7e66f2cfe05f84f3d1b50a3
DESKTOP-P4K92SG\Shinelon <fight2escape@gmail.com>
2017-10-07 12:23:06 +0800

    优化

--- Contest/687. Longest Univalue Path.h
+++ Contest/687. Longest Univalue Path.h
@@ -10,48 +10,31 @@
 class Solution {
 public:
     int longestUnivaluePath(TreeNode* root) {
-        int* res = __longestUnivaluePath(root);
-        int ret = max(res[0], max(res[1], res[2]));
-        return max(0,ret-1);
+        int grup = 0;
+        if(root) __longestUnivaluePath(root, grup);
+        return grup;
     }
 private:
-    int* __longestUnivaluePath(TreeNode* root){
-        int* include = new int[3]{0};
-        if(root==NULL){
-            return include;
-        }
-        int res = 0;
-        int* left = __longestUnivaluePath(root->left);
-        int* right = __longestUnivaluePath(root->right);
-        if(root->left && root->val == root->left->val){
-            res = left[0];
-        }
-        if(root->right && root->val == root->right->val){
-            res = max(res, right[0]);
-        }
-        res++;
-        include[0] = res;
+    int __longestUnivaluePath(TreeNode* root, int& grup){
+        int left = root->left ? __longestUnivaluePath(root->left, grup) : 0;
+        int right = root->right ? __longestUnivaluePath(root->right, grup) : 0;
+        int resl = root->left && root->left->val == root->val ? left+1 : 0;
+        int resr = root->right && root->right->val == root->val ? right+1 : 0;
+        grup = max(grup, resl + resr);
+        return max(resl, resr);
+    }
+};
 
-        res = max(left[0], max(left[1],left[2]));
-        res = max(res, max(right[0], max(right[1], right[2])));
-        include[1] = res;
-
-        res = 0;
-        if(root->left && root->right && root->val==root->left->val && root->val==root->right->val){
-            res = 1 + left[0] + right[0];
-        }
-        include[2] = res;
-
-        return include;
+class Test{
+public:
+    void test(){
+//        int arr[] = {1,4,5,4,4,NULL,5};
+        int arr[] = {1,1,1,1,1,NULL,5};
+        int n = sizeof(arr)/sizeof(int);
+        TreeNode* root = CreateBinaryTree(arr, n);
+        int res = Solution().longestUnivaluePath(root);
+        cout<<res<<endl;
     }
 };
 
 #endif //ALGORITHM_INTERVIEW2_687_LONGEST_UNIVALUE_PATH_H
-//    int arr[] = {};
-//    int arr[] = {1,4,5,4,4,5};
-//    int arr[] = {5,4,5,1,1,5};
-//    int arr[] = {1,4,5,4,4,NULL,5};
-//    int n = sizeof(arr)/sizeof(int);
-//    TreeNode* root = CreateBinaryTree(arr, n);
-//    int res = Solution().longestUnivaluePath(root);
-//    cout<<res<<endl;
